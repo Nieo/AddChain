b0db2ff829611858b765dfabeeaa3c621c3aacfc
Erik Pihl <erik.pihl@live.se>
2017-11-14 10:34:20 +0100

    Added some null guards and a link from print to build

--- addchain-app/src/app/models/build.ts
+++ addchain-app/src/app/models/build.ts
@@ -2,11 +2,22 @@ export class Build {
   build_id: number;
   material: string;
   comment: string;
-  relatedDesigns: any = [];
-  relatedPrints: any = [];
+  relatedDesigns: RelatedDesign[] = [];
+  relatedPrints: RelatedPrint[] = [];
   constructor(build_id: number, material: string, comment: string){
     this.build_id = build_id;
     this.material = material;
     this.comment = comment;
   }
+}
+
+export class RelatedDesign{
+  id: number;
+  copies: number;
+  name: string;
+}
+
+export class RelatedPrint{
+  slm_id: number;
+  start_time: Date;
 }

1a651a633a61d22ecd42c087cd24ad3c4863d7d5
Niclas Alexandersson <alniniclas@gmail.com>
2017-11-23 16:12:02 +0100

    Made search results more informative (and visually appealing).

--- addchain-server/controllers/searchcontroller.js
+++ addchain-server/controllers/searchcontroller.js
@@ -13,13 +13,17 @@ router.get('/:id', (req, res) => {
   }
   Promise.all([buildHandle.getBuildIfExists(req.params.id),
     designHandle.getDesignIfExists(req.params.id),
-    printHandle.getPrintIfExists(req.params.id)])
+    printHandle.getPrintIfExists(req.params.id),
+    buildHandle.getRelatedDesigns(req.params.id)])
     .then(data => {
         let results = {
             'builds': data[0] ? [data[0]] : [],
             'designs': data[1] ? [data[1]] : [],
             'prints': data[2] ? [data[2]] : [],
         };
+        if (results.builds.length > 0) {
+          results.builds[0].relatedDesigns = data[3];
+        }
         console.log(results);
         res.json(results);
     })

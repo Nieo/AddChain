a69ffb91d8e68effc830716ea0c7c9ffae768d35
Erik Pihl <erik.pihl@live.se>
2017-12-14 16:34:34 +0100

    Improved the viewer in many ways. Load correctly when changing to design view. More reasonable size. Some rotation of qube as default.

--- addchain-app/src/app/components/viewer/viewer.component.ts
+++ addchain-app/src/app/components/viewer/viewer.component.ts
@@ -3,6 +3,7 @@ import {GLES20} from "./GLES20_Wrapper";
 import {ViewerRenderer} from "./ViewerRenderer";
 import {DataStorage} from "./DataStorage";
 
+declare var THREE: any;
 @Component({
   selector: 'app-viewer',
   templateUrl: './viewer.component.html',
@@ -21,6 +22,69 @@ export class ViewerComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    var w = 400, h = 400;
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(w, h);
+    var view = document.getElementById("view");
+    view.appendChild(renderer.domElement);
+
+    var camera = new THREE.PerspectiveCamera(45, w / h, 1, 1000);
+    camera.position.set(3, 3, 3);
+    var controls = new THREE.TrackballControls(camera, view);
+
+    var scene = new THREE.Scene();
+    scene.add(new THREE.AmbientLight(0x666666));
+
+    var light1 = new THREE.DirectionalLight(0xffffff);
+    light1.position.set(0, 100, 100);
+    scene.add(light1);
+
+    var light2 = new THREE.DirectionalLight(0xffffff);
+    light2.position.set(0, -100, -100);
+    scene.add(light2);
+
+    var mat = new THREE.MeshPhongMaterial({
+      color: 0x339900, ambient: 0x339900, specular: 0x030303,
+    });
+    var obj = new THREE.Mesh(new THREE.Geometry(), mat);
+    scene.add(obj);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if(this.readyState === 4 && this.status === 200) {
+        openFile(xhttp.response);
+      }
+    };
+    xhttp.open("GET", "/assets/stl-reader-3.0.1/test/cube.stl",true);
+    xhttp.responseType = "blob";
+    xhttp.send();
+
+    var loop = function loop() {
+      requestAnimationFrame(loop);
+      controls.update();
+      renderer.clear();
+      renderer.render(scene, camera);
+    };
+    loop();
+
+    // file load
+    var openFile = function (file) {
+      console.log("Open file");
+      console.log(file);
+      var reader = new FileReader();
+      reader.addEventListener("load", function (ev: any) {
+        var buffer = ev.target.result;
+        var geom = loadStl(buffer);
+        scene.remove(obj);
+        obj = new THREE.Mesh(geom, mat);
+        scene.add(obj);
+      }, false);
+      reader.readAsArrayBuffer(file);
+    };
+
+
+
     // let canvas = <HTMLCanvasElement> document.getElementById('stl-viewer');
     //
     // const gl = canvas.getContext("webgl");
